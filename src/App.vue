9628a181ea6d2f19c1316107025df9558bcd1184
fozzy056 <37544663+fozzy056@users.noreply.github.com>
2020-07-13 14:58:10 +0200

    NavBar added, that's better :)

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,25 @@
 <template>
-  <div id="app">
-
-  <!-- <Footer msg="Est ce que c'est bô ?" /> -->
-
-  <div id="nav">
-    <router-link to="/">Accueil</router-link> |
-    <router-link to="/about">A propos</router-link> |
-    <router-link to="/pagedetest">Test</router-link>
+  <div>
+    <NavBar />
+    <div id="app">
+      <!-- <Footer msg="Est ce que c'est bô ?" /> -->
+      <router-view />
+    </div>
+    <Footer />
   </div>
-  <router-view/>
-
-<Footer />
-
-</div>
 </template>
 
 <script>
 import Footer from "./components/Footer.vue";
+import NavBar from "./components/NavBar";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    Footer
+    Footer,
+    NavBar
   }
-}
+};
 </script>
 
 <style lang="scss">
@@ -33,6 +29,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
